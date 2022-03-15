aadaded4dccfee430184f932b7c778872d97513d
fordim <dmitriy.formos@skyeng.ru>
2022-03-15 11:21:59 +0300

    FormArray, FormControl

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
-import {Observable, of} from "rxjs";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from "@angular/forms";
+import { Observable, of } from "rxjs";
 
 @Component({
   selector: 'app-form',
@@ -10,16 +16,49 @@ import {Observable, of} from "rxjs";
 export class FormComponent implements OnInit {
 
   public nameControl!: FormControl;
+  public fullNameControl!: FormGroup;
+  public userListControl!: FormGroup;
+  public userList!: FormArray;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    this.nameControl = new FormControl('John', [Validators.required, myValidator(3)]);
+    this.nameControl = new FormControl('John', [Validators.required, myValidator(3)], []);
     this.nameControl.valueChanges.subscribe((value => console.log(value)));
     this.nameControl.statusChanges.subscribe((status) => {
       console.log(this.nameControl.errors);
       console.log(status);
     });
+
+    this.fullNameControl = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl()
+    });
+
+    this.fullNameControl.valueChanges.subscribe((value => console.log(value)));
+
+    // Аналогичная запись, ниже через formBuilder более читабельная
+    // this.userListControl = new FormGroup({
+    //   users: new FormArray([
+    //     new FormControl('Alice'),
+    //     new FormControl('Jack'),
+    //     new FormControl('Mark'),
+    //   ])
+    // });
+
+    this.userListControl = this.formBuilder.group({
+      users: this.formBuilder.array([ ['Alice'], ['Jack'], ['Mark'] ])
+    })
+
+    this.userList = (this.userListControl.get('users') as FormArray);
+  }
+
+  removeUserControl(index: number): void {
+    this.userList.removeAt(index);
+  }
+
+  addUserControl(): void {
+    this.userList.controls.push(new FormControl(''));
   }
 }
 
@@ -34,7 +73,7 @@ function myAsycValidator(formControl: FormControl): Observable<null | any> {
   return of(null);
 }
 
-function myValidator(number:number) {
+function myValidator(number: number) {
   return function (formControl: FormControl) {
     if (formControl.value.length < number) {
       console.log('here');
